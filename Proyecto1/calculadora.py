a5b821e2e8a1b5e44127536cdb6423e8bb97a013
Carlos5f5 <carlossebas.873@gmail.com>
2024-12-20 01:54:37 -0500

    Calculadora Dos Numeros

---
+++ Proyecto1/calculadora.py
@@ -0,0 +1,31 @@
+nombre = str(input("Ingresa tu nombre: "))
+
+calculadora = "Ok " + nombre+  " Escoge una operación: \n1. Suma\n2. Resta\n3. Multiplicación\n4. División\n5. Salir\n"
+print(calculadora)
+opcion = int(input("Opción: "))
+
+while opcion != 5:
+    if opcion == 1:
+        num1 = int(input("Número 1: "))
+        num2 = int(input("Número 2: "))
+        print(f"Resultado: {num1 + num2}")
+    elif opcion == 2:
+        num1 = int(input("Número 1: "))
+        num2 = int(input("Número 2: "))
+        print(f"Resultado: {num1 - num2}")
+    elif opcion == 3:
+        num1 = int(input("Número 1: "))
+        num2 = int(input("Número 2: "))
+        print(f"Resultado: {num1 * num2}")
+    elif opcion == 4:
+        num1 = int(input("Número 1: "))
+        num2 = int(input("Número 2: "))
+        print(f"Resultado: {num1 / num2}")
+    elif opcion == 5:
+        print("Adiós")
+    else:
+        print("Opción no válida, intenta de nuevo")
+    print(calculadora)
+    opcion = int(input("Opción: "))
+    
+    break
