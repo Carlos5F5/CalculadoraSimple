fd53d9729fddeae1f72796dbb9952edd44bebba5
Carlos5f5 <carlossebas.873@gmail.com>
2025-03-26 19:41:31 -0500

    Excepcion de errores y funciones

--- Sesion7/feedback.py
+++ Sesion7/feedback.py
@@ -8,8 +8,8 @@
 #Las funciones representan las acciones de la realidad
 
 def caminar():
-    print("La persona está caminando")
-
+    #print("La persona está caminando")
+    return "La persona está caminando"
 def reir():
     print("La persona está riendo")
 
@@ -19,6 +19,26 @@ def dormir():
 #Para ejecutar la funcion se escribe el nombre de la funcion seguido de parentesis
 #Se tiene que invocar/llamar a la funcion para que se ejecute
 
-caminar()
+def suma(valor1, valor2, valor3):
+    r = valor1 + valor2 + valor3
+    p = r / 3
+    return r
+
+try:
+    n1 = int(input("Ingrese número 1: "))
+    n2 = int(input("Ingrese número 2: "))
+    n3 = int(input("Ingrese número 3: "))
+    r = suma(n1, n2, n3)
+   
+except:
+    print("Error: Ingrese solo números")
+    
+
+print ("El resultado de la suma es: ", r)
+print ("El promedio de la suma es: ", r/3)
+    
+print(caminar())
 reir()
 dormir()
+
+
